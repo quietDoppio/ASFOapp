f4413b1240c6770a2a12eb8191672e29079358de
quietDoppio <quietDoppio@yandex.ru>
2025-02-13 15:08:56 +0300

    TEST-03 testValue == 0

--- app/src/main/java/com/example/asfoapp/MainActivity.kt
+++ app/src/main/java/com/example/asfoapp/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
             ?: throw IllegalStateException("_binding for ActivityMainBinding must not be null")
     val testValue = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
