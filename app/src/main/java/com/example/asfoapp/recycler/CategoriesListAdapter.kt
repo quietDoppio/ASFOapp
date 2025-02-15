696e35732b00311923008acebe1ca22e6e6b3491
vlads2 <quietDoppio@yandex.ru>
2025-02-15 20:03:23 +0300

    RA-08 отредактировал Adapter. Добавил try catch для отлова ошибки ненайдённой картинки

--- app/src/main/java/com/example/asfoapp/recycler/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/asfoapp/recycler/CategoriesListAdapter.kt
@@ -1,18 +1,24 @@
 package com.example.asfoapp.recycler
 
 import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.asfoapp.R
 import com.example.asfoapp.data.Category
 import com.example.asfoapp.databinding.ItemCategoryBinding
 
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     Adapter<CategoriesListAdapter.CategoryItemViewHolder>() {
 
-    class CategoryItemViewHolder(private val binding: ItemCategoryBinding) :
+    class CategoryItemViewHolder(binding: ItemCategoryBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        private val cardImage = binding.cardViewImage
+        private val cardTitle = binding.cardViewTitle
+        private val cardDescription = binding.cardViewDescription
+
         companion object {
             fun inflateFrom(parent: ViewGroup): CategoryItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
@@ -22,11 +28,16 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
         }
 
         fun bind(item: Category) {
-            binding.cardViewTitle.text = item.title
-            binding.cardViewDescription.text = item.description
-            binding.cardViewImage.setImageDrawable(
-                Drawable.createFromStream(this.itemView.context.assets.open(item.imageUrl), null)
-            )
+            cardTitle.text = item.title
+            cardDescription.text = item.description
+            try {
+                cardImage.setImageDrawable(
+                    Drawable.createFromStream(itemView.context.assets.open(item.imageUrl), null)
+                )
+            } catch (e: Exception) {
+                Log.i("CategoriesListAdapter", "bind: image for ${item.imageUrl} not found")
+                cardImage.setImageResource(R.drawable.img_error)
+            }
         }
     }
 
