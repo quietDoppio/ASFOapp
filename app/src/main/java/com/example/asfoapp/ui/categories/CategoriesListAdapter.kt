08d07ea245c7c38408d9824d066fe3619a3413f1
vlads2 <quietDoppio@yandex.ru>
2025-02-16 17:23:29 +0300

    RA-08 Упростил код ViewHolder'а. Добавил content description для новых кортинок и в xlm для неизменяемых

--- app/src/main/java/com/example/asfoapp/ui/categories/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/asfoapp/ui/categories/CategoriesListAdapter.kt
@@ -3,28 +3,17 @@ package com.example.asfoapp.ui.categories
 import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
-import com.example.asfoapp.R
 import com.example.asfoapp.data.Category
 import com.example.asfoapp.databinding.ItemCategoryBinding
 
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     Adapter<CategoriesListAdapter.CategoryItemViewHolder>() {
 
-    class CategoryItemViewHolder(view: View) :
-        RecyclerView.ViewHolder(view) {
-        private val binding = ItemCategoryBinding.bind(view)
-
-        companion object {
-            fun inflateFrom(parent: ViewGroup): CategoryItemViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.item_category, parent, false)
-                return CategoryItemViewHolder(view)
-            }
-        }
+    class CategoryItemViewHolder(private val binding: ItemCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Category) {
             binding.cardViewTitle.text = item.title
@@ -33,20 +22,21 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
                 val inputStream = itemView.context.assets.open(item.imageUrl)
                 val image = Drawable.createFromStream(inputStream, null)
                 binding.cardViewImage.setImageDrawable(image)
+                binding.cardViewImage.contentDescription = "${item.title} - ${item.description}"
             } catch (e: Exception) {
                 val stackTrace = Log.getStackTraceString(e)
                 Log.e(
                     "CategoriesListAdapter",
                     "Image - ${item.imageUrl} not found in assets\n$stackTrace"
                 )
-                binding.cardViewImage.setImageResource(R.drawable.img_error)
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryItemViewHolder {
-        val holder = CategoryItemViewHolder.inflateFrom(parent)
-        return holder
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemCategoryBinding.inflate(layoutInflater, parent, false)
+        return CategoryItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CategoryItemViewHolder, position: Int) {
