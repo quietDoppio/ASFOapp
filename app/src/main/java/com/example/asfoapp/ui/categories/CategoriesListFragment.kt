909234715197572ee374a36af36027701366b554
vlads2 <quietDoppio@yandex.ru>
2025-02-19 15:37:39 +0300

    RA-10 Реализовал прередачу данных из адаптера recyclerView во фрагмент при клике на итем вью и навигации

--- app/src/main/java/com/example/asfoapp/ui/categories/CategoriesListFragment.kt
+++ app/src/main/java/com/example/asfoapp/ui/categories/CategoriesListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -40,18 +41,26 @@ class CategoriesListFragment : Fragment() {
         val adapter = CategoriesListAdapter(categoriesList)
         adapter.setOnItemClickListener(
             object : CategoriesListAdapter.OnItemClickListener {
-                override fun onItemClick() {
-                    openRecipesByCategoryId()
+                override fun onItemClick(categoryId: Int) {
+                    openRecipesByCategoryId(categoryId)
                 }
             }
         )
         binding.rvCategories.adapter = adapter
     }
 
-    private fun openRecipesByCategoryId() {
+    private fun openRecipesByCategoryId(categoryId: Int) {
+        val category = STUB.getCategories()
+        val categoryName = category.find { it.id == categoryId }?.title
+        val categoryImageUrl = category.find { it.id == categoryId }?.imageUrl
         requireActivity().supportFragmentManager.commit {
+            val bundle = bundleOf(
+                "ARG_CATEGORY_ID" to categoryId,
+                "ARG_CATEGORY_NAME" to categoryName,
+                "ARG_CATEGORY_IMAGE_URL" to categoryImageUrl
+            )
             setReorderingAllowed(true)
-            replace<RecipesListFragment>(R.id.mainContainer)
+            replace<RecipesListFragment>(R.id.mainContainer, args = bundle)
             addToBackStack("CategoriesListFragment")
         }
     }
--- app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListFragment.kt
+++ app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListFragment.kt
@@ -13,11 +13,20 @@ class RecipesListFragment : Fragment() {
         get() = _binding
             ?: throw IllegalStateException("binding for RecipesListFragment must not be null")
 
+    private var categoryId: Int? = null
+    private var categoryName: String? = null
+    private var categoryImageUrl: String? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRecipesListBinding.inflate(layoutInflater, container, false)
         val view = binding.root
+        categoryId = requireArguments().getInt("ARG_CATEGORY_ID")
+        categoryName = requireArguments().getString("ARG_CATEGORY_NAME")
+        categoryImageUrl = requireArguments().getString("ARG_CATEGORY_IMAGE_URL")
+
+        binding.testText.text = "id: $categoryId\ntitle - $categoryName\nimage - $categoryImageUrl"
 
         return view
     }
