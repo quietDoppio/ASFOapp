1433d4ad1df77432cddcba60d17a11f88fd1d655
quietDoppio <quietDoppio@yandex.ru>
2025-02-20 00:09:31 +0300

    RA-11 определил интерфес для навигации по картачкам рецептов в RecipesListAdapter

--- app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListAdapter.kt
+++ app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListAdapter.kt
@@ -11,21 +11,35 @@ import com.example.asfoapp.databinding.ItemRecipeBinding
 
 class RecipesListAdapter(private val dataSet: List<Recipe>) :
     Adapter<RecipesListAdapter.RecipeItemViewHolder>() {
+    interface OnItemClickListener {
+        fun onItemClick(recipeId: Int)
+    }
+
+    private var itemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        itemClickListener = listener
+    }
 
     class RecipeItemViewHolder(private val binding: ItemRecipeBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: Recipe) {
+
+        fun bind(item: Recipe, itemClickListener: OnItemClickListener?) {
             binding.recipeName.text = item.title
             try {
                 val inputStream = itemView.context.assets.open(item.imageUrl)
                 val image = Drawable.createFromStream(inputStream, null)
                 binding.recipeImage.setImageDrawable(image)
+                binding.recipeImage.contentDescription = "$item.title"
             } catch (e: Exception) {
                 val stackTrace = Log.getStackTraceString(e)
                 Log.e(
                     "RecipesListAdapter",
                     "Image - ${item.imageUrl} not found in assets\n$stackTrace"
                 )
+            }
+            binding.root.setOnClickListener {
+                itemClickListener?.onItemClick(item.id)
             }
         }
     }
@@ -40,6 +54,6 @@ class RecipesListAdapter(private val dataSet: List<Recipe>) :
 
     override fun onBindViewHolder(holder: RecipeItemViewHolder, position: Int) {
         val item = dataSet[position]
-        holder.bind(item)
+        holder.bind(item, itemClickListener)
     }
 }
