e6d97611feeb2b6fcb5492421cf114f5ff5000ee
vlads2 <quietDoppio@yandex.ru>
2025-02-19 18:04:01 +0300

    RA-11 Вёрстка экрана рецептов

--- app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListFragment.kt
+++ app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListFragment.kt
@@ -1,6 +1,8 @@
 package com.example.asfoapp.ui.categories.recipes
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +28,18 @@ class RecipesListFragment : Fragment() {
         categoryName = requireArguments().getString("ARG_CATEGORY_NAME")
         categoryImageUrl = requireArguments().getString("ARG_CATEGORY_IMAGE_URL")
 
-        binding.testText.text = "id: $categoryId\ntitle - $categoryName\nimage - $categoryImageUrl"
+        try {
+            val inputStream = requireContext().assets.open(categoryImageUrl!!)
+            val drawable = Drawable.createFromStream(inputStream, null)
+            binding.categoryImage.setImageDrawable(drawable)
+            binding.categoryName.text = categoryName
+        } catch (e: Exception) {
+            val stackTrace = Log.getStackTraceString(e)
+            Log.e(
+                "RecipesListFragment",
+                "Image - $categoryImageUrl not found in assets\n$stackTrace"
+            )
+        }
 
         return view
     }
