b41044fed6138c12730a908b5ee4756210437f5a
vlads2 <quietDoppio@yandex.ru>
2025-02-20 10:37:24 +0300

    RA-11 Реализовал переход на фрагмент рецепта

--- app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListFragment.kt
+++ app/src/main/java/com/example/asfoapp/ui/categories/recipes/RecipesListFragment.kt
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.asfoapp.R
+import com.example.asfoapp.data.Recipe
 import com.example.asfoapp.data.STUB
 import com.example.asfoapp.databinding.FragmentRecipesListBinding
 
@@ -34,25 +38,50 @@ class RecipesListFragment : Fragment() {
 
         return view
     }
-    private fun initRecycler(){
-        categoryId?.let {
-            val adapter = RecipesListAdapter(STUB.getRecipesByCategoryId(it))
+
+    private fun initRecycler() {
+        categoryId?.let { categoryId ->
+            val recipesList = STUB.getRecipesByCategoryId(categoryId)
+            val adapter = RecipesListAdapter(recipesList)
+            adapter.setOnItemClickListener(
+                object : RecipesListAdapter.OnItemClickListener {
+                    override fun onItemClick(recipeId: Int) {
+
+                        openRecipeByRecipeId(recipesList, recipeId)
+                    }
+                }
+            )
             binding.rvRecipes.adapter = adapter
         }
 
     }
-    private fun setInfo(){
+    private fun openRecipeByRecipeId(recipesList: List<Recipe>, recipeId: Int){
+        val recipe = recipesList.find { it.id == recipeId }
+        recipe?.let {  it ->
+            val recipeName = it.title
+            val recipeIngredients = it.ingredients
+            val recipeMethod = it.method
+            val recipeImageUrl = it.imageUrl
+        }
+        requireActivity().supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<RecipeFragment>(R.id.mainContainer)
+            addToBackStack("RecipesFragment")
+        }
+    }
+
+    private fun setInfo() {
         binding.categoryName.text = categoryName
-        categoryImageUrl?.let { image ->
+        categoryImageUrl?.let { imageUrl ->
             try {
-                val inputStream = requireContext().assets.open(image)
-                val drawable = Drawable.createFromStream(inputStream, null)
-                binding.categoryImage.setImageDrawable(drawable)
+                val inputStream = requireContext().assets.open(imageUrl)
+                val image = Drawable.createFromStream(inputStream, null)
+                binding.categoryImage.setImageDrawable(image)
             } catch (e: Exception) {
                 val stackTrace = Log.getStackTraceString(e)
                 Log.e(
                     "RecipesListFragment",
-                    "Image - $image not found in assets\n$stackTrace"
+                    "Image - $imageUrl not found in assets\n$stackTrace"
                 )
             }
         }
