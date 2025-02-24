76e3a615762580b584889e82c5624987a016f31f
quietDoppio <quietDoppio@yandex.ru>
2025-02-25 01:47:16 +0300

    RA-13 добавил разделители для списов recyclerView

--- app/src/main/java/com/example/asfoapp/ui/recipes/RecipeFragment.kt
+++ app/src/main/java/com/example/asfoapp/ui/recipes/RecipeFragment.kt
@@ -8,10 +8,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
+import com.example.asfoapp.R
 import com.example.asfoapp.data.Recipe
 import com.example.asfoapp.databinding.FragmentRecipeBinding
 import com.example.asfoapp.ui.recipes.adapters.IngredientsAdapter
 import com.example.asfoapp.ui.recipes.adapters.MethodAdapter
+import com.google.android.material.divider.MaterialDividerItemDecoration
 
 class RecipeFragment : Fragment() {
     private var _binding: FragmentRecipeBinding? = null
@@ -26,14 +29,14 @@ class RecipeFragment : Fragment() {
         _binding = FragmentRecipeBinding.inflate(layoutInflater, container, false)
         val view = binding.root
 
-        recipe = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+        recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             requireArguments().getParcelable(ARG_RECIPE, Recipe::class.java)
         } else {
             requireArguments().getParcelable(ARG_RECIPE) as? Recipe
         }
 
         setContentView()
-        initAdapter()
+        initRecycler()
         return view
     }
 
@@ -41,13 +44,14 @@ class RecipeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-    private fun setContentView(){
+
+    private fun setContentView() {
         recipe?.let { recipe ->
             binding.recipeTitle.text = recipe.title
             try {
-            val inputStream = requireContext().assets.open(recipe.imageUrl)
-            val image = Drawable.createFromStream(inputStream, null)
-            binding.recipeImage.setImageDrawable(image)
+                val inputStream = requireContext().assets.open(recipe.imageUrl)
+                val image = Drawable.createFromStream(inputStream, null)
+                binding.recipeImage.setImageDrawable(image)
             } catch (e: Exception) {
                 val stackTrace = Log.getStackTraceString(e)
                 Log.e(
@@ -57,13 +61,23 @@ class RecipeFragment : Fragment() {
             }
         }
     }
-    private fun initAdapter(){
+
+    private fun initRecycler() {
         recipe?.let {
-            Log.i("initAdapter", "recipe.method - ${it.method}", )
+            Log.i("initAdapter", "recipe.method - ${it.method}")
             val ingredientsAdapter = IngredientsAdapter(it.ingredients)
             val methodAdapter = MethodAdapter(it.method)
+            val divider = MaterialDividerItemDecoration(
+                requireContext(), VERTICAL
+            ).apply {
+                setDividerInsetEndResource(requireContext(), R.dimen.spacing_medium_8dp)
+                setDividerInsetStartResource(requireContext(), R.dimen.spacing_medium_8dp)
+                setDividerColorResource(requireContext(), R.color.figma_gray_light)
+            }
             binding.rvIngredients.adapter = ingredientsAdapter
+            binding.rvIngredients.addItemDecoration(divider)
             binding.rvMethod.adapter = methodAdapter
+            binding.rvMethod.addItemDecoration(divider)
         }
 
     }
