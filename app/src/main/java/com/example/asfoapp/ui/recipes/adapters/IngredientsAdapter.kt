a83b0c75ba64acf942adc4a6d8354b4238d36a86
quietDoppio <quietDoppio@yandex.ru>
2025-02-27 22:15:16 +0300

    RA-14 улучшил функцию слушателя seekBar и вынес логику изменения значений quantity в адаптер

--- app/src/main/java/com/example/asfoapp/ui/recipes/adapters/IngredientsAdapter.kt
+++ app/src/main/java/com/example/asfoapp/ui/recipes/adapters/IngredientsAdapter.kt
@@ -2,17 +2,19 @@ package com.example.asfoapp.ui.recipes.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.isDigitsOnly
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.asfoapp.data.Ingredient
 import com.example.asfoapp.databinding.ItemIngredientBinding
 
 class IngredientsAdapter(dataSet: List<Ingredient>) : Adapter<IngredientsAdapter.IngredientsItemViewHolder>(){
-    var dataSet:List<Ingredient> = dataSet
+    private var dataSet:List<Ingredient> = dataSet
         set(value) {
             field = value
             notifyDataSetChanged()
         }
+    private val defaultQuantities = dataSet.map { it.quantity }
 
     class IngredientsItemViewHolder(private val binding: ItemIngredientBinding) : ViewHolder(binding.root){
         fun bind(item: Ingredient){
@@ -33,5 +35,15 @@ class IngredientsAdapter(dataSet: List<Ingredient>) : Adapter<IngredientsAdapter
     override fun onBindViewHolder(holder: IngredientsItemViewHolder, position: Int) {
        val item = dataSet[position]
         holder.bind(item)
+    }
+    fun updateIngredientsQuantity(progress: Int){
+          val newIngredients = dataSet.mapIndexed { index, ingredient ->
+               if(defaultQuantities[index].isDigitsOnly()){
+                   ingredient.copy(quantity = (defaultQuantities[index].toInt() * 10).toString())
+               } else {
+                   ingredient.copy(quantity = (defaultQuantities[index].toDouble() * progress).toString())
+               }
+           }
+        dataSet = newIngredients
     }
 }
