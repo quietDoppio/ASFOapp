e6703dd46adcea6e688a7bde9c7330118157a643
quietDoppio <quietDoppio@yandex.ru>
2025-05-01 14:52:48 +0300

    RA-33 Создал пул потоков, получил списки рецептов каждой категории

--- app/src/main/java/com/example/asfoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/asfoapp/ui/MainActivity.kt
@@ -8,27 +8,18 @@ import androidx.navigation.navOptions
 import com.example.asfoapp.R
 import com.example.asfoapp.databinding.ActivityMainBinding
 import com.example.asfoapp.model.Category
+import com.example.asfoapp.model.Recipe
 import kotlinx.serialization.json.Json
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding
         get() = _binding
             ?: throw IllegalStateException("_binding for ActivityMainBinding must not be null")
-
-    private val thread = Thread {
-        Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}} ")
-        val url = URL("https://recipes.androidsprint.ru/api/category")
-        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-        connection.connect()
-
-        val body = connection.inputStream.bufferedReader().readText()
-        Log.i("!!!", "body: $body")
-        val categoryList = Json.decodeFromString<List<Category>>(body)
-        Log.i("!!!", "category list: ${categoryList.joinToString(", ") { it.title }}")
-    }
+    private val threadPool = Executors.newFixedThreadPool(10)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +28,40 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        thread.start()
+        threadPool.execute{
+            Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}} ")
+            val urlPath = "https://recipes.androidsprint.ru/api"
+            val getCategoriesUrl = URL("$urlPath/category")
+
+            var connection: HttpURLConnection = getCategoriesUrl.openConnection() as HttpURLConnection
+            connection.connect()
+            val categoriesResponseBody = connection.inputStream.bufferedReader().readText()
+            connection.disconnect()
+
+            Log.i("!!!", "body: $categoriesResponseBody")
+
+            val categoryList = Json.decodeFromString<List<Category>>(categoriesResponseBody)
+            val categoryIds = categoryList.map { it.id }
+            val recipesList: MutableMap<Int ,List<Recipe>> = mutableMapOf()
+
+            categoryIds.forEach { id ->
+                connection = URL("$urlPath/category/$id/recipes").openConnection() as HttpURLConnection
+                connection.connect()
+                val recipesListResponseBody = connection.inputStream.bufferedReader().readText()
+                val recipes = Json.decodeFromString<List<Recipe>>(recipesListResponseBody)
+                recipesList[id] = recipes
+                connection.disconnect()
+            }
+            val recipesListString = """
+                |recipes:
+                |${recipesList.map {
+                        "category id - ${it.key}, ${it.value.joinToString(", ") {str -> str.title }}"
+                    }.joinToString("\n")}
+            """.trimMargin("|")
+            Log.i("!!!", "categories list: ${categoryList.joinToString(", ") { it.title }}")
+            Log.i("!!!", recipesListString)
+
+        }
 
         val navController =
             (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
@@ -66,5 +90,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        threadPool.shutdown()
     }
 }
