58409f5a3869abce0a2964015ad20ed77effb444
quietDoppio <quietDoppio@yandex.ru>
2025-12-02 20:49:38 +0300

    RA-45-di. перенёс градл зависимости в libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     kotlin("plugin.serialization") version "2.1.20" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.9.0" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.6" apply false
     id("com.google.devtools.ksp") version "2.1.20-2.0.1" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
 }
